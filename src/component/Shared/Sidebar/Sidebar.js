b557d223cebdf78a3e140b806621425871763a2b
Mohammad Osman Goni <osmangoni0827@gmail.com>
2021-06-26 23:05:20 +0600

    update

--- src/component/Shared/Sidebar/Sidebar.js
+++ src/component/Shared/Sidebar/Sidebar.js
@@ -6,6 +6,7 @@ import './Sidebar.css';
 import { loggedInContext } from '../../../App';
 const Sidebar = () => {
     const [loggedInUser, setLoggedInUser] = useContext(loggedInContext);
+    const [isAdmin,setAdmin]=useState(false);
     const email=loggedInUser.email||sessionStorage.getItem('email');
     const password=loggedInContext.password||sessionStorage.getItem('password');
     const history=useHistory();
@@ -22,43 +23,42 @@ const Sidebar = () => {
                 console.log(data)
                 alert("You Are Not Admin. Your Permission Not Alow");
                 history.push('/home');
+                }else{
+                    setAdmin(true)
                 }
             })
     }, [loggedInUser.email])
    
     return (
-        
-            <div className="sidebar sticky-top">
-                <div className="d-flex justify-content-center">
-                <img src="https://i.ibb.co/0mP8GZv/1j-oj-FVDOMk-X9-Wytexe43-D6khv-WAq-RFNk-B3-Iw-Xs1-M3-EMo-AJtli-Ipgv-Bq9-U.png" alt=""></img>
-                </div>
-                <Link to="/dashboard">
-                <div className='d-flex align-items-center dashboard'>
-                <FontAwesomeIcon className='icon' icon={faBorderNone} size='2x' color="#00000" />
-                <h3>Dashboard</h3>
-                </div>
-                </Link>
-                <ul className="navbar-nav ">
-                    
-                    <li className="nav-item menu d-flex  align-items-center  p-2">
-                    <FontAwesomeIcon className='icon' icon={faTasks} size='2x' color="#FFFFFF" />
-                        <NavLink activeClassName="active" className="nav-link " to='/manageblog'>Manage Blog</NavLink>
-                    </li>
-                    <li className="nav-item menu d-flex  align-items-center p-2 ">
-                    <FontAwesomeIcon className='icon' icon={faPlusCircle} size='2x' color="#FFFFFF" />
-
-                        <NavLink activeClassName="active" className="nav-link " to='/addNewBlog'>Add Blog</NavLink>
-                    </li>
-
-                   
-                    <li className="nav-item menu d-flex  align-items-center  p-2">
-                    <FontAwesomeIcon className='icon' icon={faUsersCog} size='2x' color="#FFFFFF" />
-                        <NavLink activeClassName="active" className="nav-link " to='/makeAdmin'>Create New Admin</NavLink>
-                    </li>
-                </ul>
+    isAdmin&&(<div className="sidebar sticky-top">
+            <div className="d-flex justify-content-center">
+            <img src="https://i.ibb.co/0mP8GZv/1j-oj-FVDOMk-X9-Wytexe43-D6khv-WAq-RFNk-B3-Iw-Xs1-M3-EMo-AJtli-Ipgv-Bq9-U.png" alt=""></img>
             </div>
+            <Link to="/dashboard">
+            <div className='d-flex align-items-center dashboard'>
+            <FontAwesomeIcon className='icon' icon={faBorderNone} size='2x' color="#00000" />
+            <h3>Dashboard</h3>
+            </div>
+            </Link>
+            <ul className="navbar-nav ">
+                
+                <li className="nav-item menu d-flex  align-items-center  p-2">
+                <FontAwesomeIcon className='icon' icon={faTasks} size='2x' color="#FFFFFF" />
+                    <NavLink activeClassName="active" className="nav-link " to='/manageblog'>Manage Blog</NavLink>
+                </li>
+                <li className="nav-item menu d-flex  align-items-center p-2 ">
+                <FontAwesomeIcon className='icon' icon={faPlusCircle} size='2x' color="#FFFFFF" />
 
+                    <NavLink activeClassName="active" className="nav-link " to='/addNewBlog'>Add Blog</NavLink>
+                </li>
 
+               
+                <li className="nav-item menu d-flex  align-items-center  p-2">
+                <FontAwesomeIcon className='icon' icon={faUsersCog} size='2x' color="#FFFFFF" />
+                    <NavLink activeClassName="active" className="nav-link " to='/makeAdmin'>Create New Admin</NavLink>
+                </li>
+            </ul>
+        </div>)
     );
 };
 
